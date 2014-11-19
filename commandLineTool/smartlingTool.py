9d42d406b7539acff89b90bb05469a85e33aacf8
mgoodnow <michael.goodnow@bazaarvoice.com>
2014-11-18 19:21:28 -0600

    Adding command line tool for get, upload, and import for file or directories
    - Updating FileApiBase.commandUpload to allow for custom URI file location
    - Updating FileApiBase.commandImport to add directives
    - Updating UploadData to include field for custom URI file location

---
+++ commandLineTool/smartlingTool.py
@@ -0,0 +1,421 @@
+#
+# Python command-line: Translation Tool
+# Contributed by: Bazaarvoice
+#
+
+import os
+import sys
+import argparse
+import logging
+import ConfigParser
+
+lib_path = os.path.abspath('../')
+sys.path.append(lib_path)  # allow to import ../smartlingApiSdk/SmartlingFileApi
+
+from smartlingApiSdk.SmartlingFileApi import SmartlingFileApiFactory
+from smartlingApiSdk.SmartlingDirective import SmartlingDirective
+from smartlingApiSdk.UploadData import UploadData
+from smartlingApiSdk.Constants import ReqMethod
+
+
+class SmartlingApi:
+    def __init__(self, apiKey, projectId):
+        self.file_api = SmartlingFileApiFactory().getSmartlingTranslationApiProd(apiKey, projectId)
+
+    # Upload a source file
+    def uploadFile(self, uploadData):
+        self.disableStdOut()
+        response, code = self.file_api.upload(uploadData)
+        self.enableStdOut()
+        if code == 200 and response.code == "SUCCESS":
+            return response.data
+        else:
+            raise IOError("Failed to upload ({0}), caused by: {1}".format(code, self._getMessages(response)))
+
+    # Import a translation
+    # ApiResponse: {"response":{"data":{"wordCount":10,"translationImportErrors":[{"contentFileId":238103,"stringHashcode":"851194c88c080f24ef257383841eb757","messages":["Information about import key was not found"],"importKey":null}],"stringCount":5},"code":"SUCCESS","messages":[]}}
+    def importFile(self, uploadData, locale, overwrite=False):
+        self.disableStdOut()
+        response, code = self.file_api.import_call(uploadData, locale, translationState="PUBLISHED", overwrite=str(overwrite).lower())
+        self.enableStdOut()
+        if code == 200 and response.code == "SUCCESS":
+            if response.data.translationImportErrors and len(response.data.translationImportErrors) > 0:
+                logging.error("Error Importing translation (%s): %s, Caused by:\n%s",
+                              locale,
+                              uploadData.uriPath + uploadData.name,
+                              self._getStringFromArray(response.data.translationImportErrors))
+                raise IOError("Failed to import translation file: {0}".format(uploadData.uriPath + uploadData.name))
+            return response.data
+        else:
+            raise IOError("Failed to import ({0}), caused by: {1}".format(code, self._getMessages(response)))
+
+    # Get a translated files
+    def getFile(self, fileUri, locale):
+        self.disableStdOut()
+        data, code = self.file_api.get(fileUri, locale)
+        self.enableStdOut()
+        if code == 200:
+            return data
+        else:
+            raise IOError("Failed to get file ({0}): {1}".format(locale, fileUri))
+
+    # Get file status
+    # ApiResponse: {"response":{"data":{"fileUri":"common.json","wordCount":9,"fileType":"json","callbackUrl":null,"lastUploaded":"2014-10-30T19:09:36","stringCount":5,"approvedStringCount":0,"completedStringCount":0},"code":"SUCCESS","messages":[]}}
+    def getStatus(self, fileUri, locale):
+        self.disableStdOut()
+        response, code = self.file_api.status(fileUri, locale)
+        self.enableStdOut()
+        if code == 200 and response.code == "SUCCESS":
+            return response.data
+        else:
+            raise IOError("Failed to get file status ({0}): {1}, caused by: {2}".format(locale, fileUri, self._getMessages(response)))
+
+    # Get list of the project locales
+    # ApiResponse: [{'locale': 'de-DE', 'translated': 'Deutsch', 'name': 'German (Germany)'}, {'locale': 'pl-PL', 'translated': 'Polski', 'name': 'Polish (Poland)'}]
+    def getProjectLocales(self):
+        self.disableStdOut()
+        response, code = self.file_api.command(ReqMethod.GET, "/v1/project/locale/list", params={})
+        self.enableStdOut()
+        if code != 200 or response.code != "SUCCESS":
+            raise IOError("Failed to get project locales, caused by: {0}".format(self._getMessages(response)))
+        return response.data.locales
+
+    def _getMessages(self, response):
+        if response and response.messages:
+            return self._getStringFromArray(response.messages)
+
+    def _getStringFromArray(self, array):
+        message = ""
+        for m in array:
+            if len(message) > 0:
+                message += "\n"
+            message += str(m)
+        return message
+
+    def disableStdOut(self):
+        self._stdout = sys.stdout
+        null = open(os.devnull, 'wb')
+        sys.stdout = null
+
+    def enableStdOut(self):
+        if self._stdout:
+            sys.stdout = self._stdout
+
+
+class SmartlingTranslations:
+    def __init__(self, args):
+        self.args = args
+        self.api = SmartlingApi(args.apiKey, args.projectId)
+
+    #
+    # Upload Source
+    #
+    def uploadSource(self, directives=None):
+        args = self.args
+        logging.info("Uploading source file(s) from: %s", args.dir)
+        if not os.path.isdir(args.dir) and not os.path.isfile(args.dir):
+            raise ValueError("Invalid source directory/file: {0}".format(args.dir))
+
+        if os.path.isfile(args.dir):
+            # Upload single file
+            self._uploadSourceFile(os.path.dirname(args.dir), os.path.basename(args.dir), args.uriPath, directives)
+        else:
+            # Loop through files in directory recursively
+            for root, dirs, files in os.walk(args.dir):
+                for name in files:
+                    relativeUri = args.uriPath + self._getRelativePath(args.dir, root)
+                    self._uploadSourceFile(root, name, relativeUri, directives)
+
+    def _uploadSourceFile(self, path, fileName, uriPath, directives=None):
+        absFile = os.path.join(path, fileName)
+        logging.debug("Uploading: %s", absFile)
+
+        if not path.endswith('/'):
+            path += "/"
+
+        if self.args.run:
+            uploadData = UploadData(path, fileName, self._getFileType(fileName), uriPath)
+
+            if directives:
+                for directive, value in directives:
+                    uploadData.addDirective(SmartlingDirective(directive, value))
+
+            stats = self.api.uploadFile(uploadData)
+            logging.info("Uploaded: %s -> %s (Word count = %s  New source = %s)",
+                         absFile,
+                         uriPath + fileName,
+                         stats.wordCount,
+                         "No" if stats.overWritten else "Yes")
+        else:
+            logging.info("Upload (noop): %s -> %s", os.path.join(path, fileName), uriPath + fileName)
+
+    #
+    # Import Translations
+    #
+    def importTranslations(self, directives=None):
+        args = self.args
+        logging.info("Importing translation file(s) from: %s", args.dir)
+        if not os.path.isdir(args.dir) and not os.path.isfile(args.dir):
+            raise ValueError("Invalid translation directory/file: {0}".format(args.dir))
+
+        if os.path.isfile(args.dir):
+            # Upload single file
+            self._importTranslationFile(os.path.dirname(args.dir), os.path.basename(args.dir), args.uriPath, args.locale, directives, args.overwrite)
+        else:
+            # Loop through files in directory recursively
+            for root, dirs, files in os.walk(args.dir):
+                for name in files:
+                    relativeUri = args.uriPath + self._getRelativePath(args.dir, root)
+                    self._importTranslationFile(root, name, relativeUri, args.locale, directives, args.overwrite)
+
+    def _importTranslationFile(self, path, fileName, uriPath, locale, directives=None, overwrite=False):
+        smartlingLocale = self._getSmartlingLocale(locale)
+
+        # Test if English source exists
+        try:
+            self.api.getStatus(uriPath + fileName, smartlingLocale)
+        except IOError:
+            logging.error("Failed to import translation as English source has not been uploaded: %s", uriPath + fileName)
+            raise ValueError("No English source: {0}".format(uriPath + fileName))
+
+        absFile = os.path.join(path, fileName)
+        if self.args.run:
+            logging.debug("Importing translation (%s): %s", smartlingLocale, absFile)
+            if not path.endswith('/'):
+                path += "/"
+            uploadData = UploadData(path, fileName, self._getFileType(fileName), uriPath)
+            uploadData.uri = uriPath + fileName
+            if directives:
+                for directive, value in directives:
+                    uploadData.addDirective(SmartlingDirective(directive, value))
+            stats = self.api.importFile(uploadData, smartlingLocale, overwrite)
+            logging.info("Imported translation (%s): %s -> %s (Word count = %s)",
+                         smartlingLocale,
+                         absFile,
+                         uriPath + fileName,
+                         stats.wordCount)
+        else:
+            logging.info("Import translation (noop): %s -> %s", absFile, uriPath + fileName)
+
+    #
+    # Get Translations
+    #
+    def getTranslations(self):
+        args = self.args
+        logging.info("Downloading translated file(s) for: %s", args.dir)
+        if not os.path.isdir(args.dir) and not os.path.isfile(args.dir):
+            raise ValueError("Invalid English source directory/file: {}".format(args.dir))
+
+        if not os.path.isdir(args.outputDir):
+            logging.debug("Creating output directory: %s", args.outputDir)
+            os.makedirs(args.outputDir)
+
+        projectLocales = self.api.getProjectLocales()
+
+        allComplete = True
+        for l in projectLocales:
+            smartlingLocale = l['locale']
+            if args.locale and args.locale != self._getLocaleFromSmartlingLocale(smartlingLocale):
+                logging.debug("Skipping locale: %s", smartlingLocale)
+                continue
+            logging.info("Fetching translations%s for %s (%s)", "" if args.run else " (noop)", l['name'], smartlingLocale)
+            if os.path.isfile(args.dir):
+                # Get single file
+                if not self._getTranslationsFile(args.outputDir, "", args.uriPath, os.path.basename(args.dir), smartlingLocale):
+                    allComplete = False
+            else:
+                # Loop through files in directory recursively and get their translations from the server respectfully
+                for root, dirs, files in os.walk(args.dir):
+                    for name in files:
+                        relativePath = self._getRelativePath(args.dir, root)
+                        if not self._getTranslationsFile(args.outputDir, relativePath, args.uriPath, name, smartlingLocale):
+                            allComplete = False
+
+        if allComplete:
+            logging.info("Successfully - all source files translated!")
+        else:
+            logging.warn("Not all source files are translated!")
+
+    def _getTranslationsFile(self, outputDir, relativePath, uriPath, fileName, smartlingLocale):
+        args = self.args
+        sourceFile = uriPath + relativePath + fileName
+        outputFile = outputDir + "/" + self._getLocaleFromSmartlingLocale(smartlingLocale) + "/" + relativePath + fileName
+
+        status = self.api.getStatus(sourceFile, smartlingLocale)
+        percentComplete = self._getPercent(status.stringCount, status.completedStringCount)
+
+        if percentComplete < 100:
+            logging.info("Translated %s%% (%s): %s -> %s (skipping)", percentComplete, smartlingLocale, sourceFile, outputFile)
+            return False
+
+        logging.info("Translated %s%% (%s): %s -> %s", percentComplete, smartlingLocale, sourceFile, outputFile)
+        if args.run:
+            fileData = self.api.getFile(sourceFile, smartlingLocale)
+            if not os.path.isdir(os.path.dirname(outputFile)):
+                os.makedirs(os.path.dirname(outputFile))
+            f = open(outputFile, 'w')
+            f.write(fileData)
+
+        return True
+
+    # Get percentage
+    def _getPercent(self, count, totalCount):
+        if totalCount > 0:
+            return int((count / totalCount) * 100)
+        return 0
+
+    # Get Smartling locale from locale
+    def _getSmartlingLocale(self, locale):
+        if self.args.localeMap:
+            if self.args.localeMap[locale]:
+                return self.args.localeMap[locale]
+        return locale
+
+    # Get locale from a Smartling locale
+    def _getLocaleFromSmartlingLocale(self, smartlingLocale):
+        if self.args.localeMap:
+            for locale, slLocale in self.args.localeMap.iteritems():
+                if slLocale == smartlingLocale:
+                    return locale
+        return smartlingLocale
+
+    # Get relative path
+    # rootDir = /root/path
+    # fileDir = /root/path/subdir/path/file
+    # returns subdir/path/file
+    def _getRelativePath(self, rootDir, filePath):
+        relativePath = filePath.replace(rootDir, "", 1)
+        if relativePath.startswith("/"):
+            relativePath = relativePath[1:]
+        if len(relativePath) > 0 and not relativePath.endswith("/"):
+            relativePath += "/"
+        return relativePath
+
+    # Auto detect Smartling file type
+    def _getFileType(self, file):
+        extension = os.path.splitext(file)[1][1:]
+        # Gettext .pot and .po files
+        if extension == "pot" or extension == "po":
+            return "gettext"
+        # HTML files
+        if extension == "html" or extension == "htm":
+            return "html"
+        # Java Properties
+        if extension == "properties":
+            return "javaProperties"
+        # Yaml files
+        if extension == "yml":
+            return "yaml"
+        # Supports .xlf, .xliff, and .xml files that use the XML Localization Interchange File Format (XLIFF)
+        if extension == ".xlf" or extension == "xliff":
+            return "xliff"
+        # Javascript files
+        if extension == "json" or extension == "js":
+            return "json"
+        # Qt Linguist TS format files
+        if extension == "ts":
+            return "qt"
+        # MadCap Flare ZIP packages
+        if extension == "zip":
+            return "madcap"
+        # All others where extension matches type
+        return extension
+
+
+def uploadSource(args, directives=None):
+    tool = SmartlingTranslations(args)
+    tool.uploadSource(directives)
+
+
+def getTranslations(args):
+    tool = SmartlingTranslations(args)
+    tool.getTranslations()
+
+
+def importTranslations(args, directives=None):
+    tool = SmartlingTranslations(args)
+    tool.importTranslations(directives)
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Smartling Translation Tool to upload, get, and import translation files")
+    parser.add_argument('-k', '--apiKey', dest="apiKey", help='Smartling API key (overrides configuration file value)')
+    parser.add_argument('-p', '--projectId', dest="projectId",
+                        help='Smartling project ID (overrides configuration file value)')
+    parser.add_argument('-c', '--config', dest="configFile", default="translation.cfg", help='Configuration file (default ./translation.cfg)')
+
+    subparsers = parser.add_subparsers(dest="sub_parser", help="To see individual sub command help: 'subcommand -h'")
+
+    parser_upload = subparsers.add_parser('upload', help='Upload the (English) source')
+    parser_upload.add_argument('-d', '--dir', dest="dir", required=True, help='Path to English source directory or file')
+    parser_upload.add_argument('-u', '--uriPath', dest="uriPath", help='File URI path used in Smartling system')
+    parser_upload.add_argument('--run', dest="run", action='store_true', help='Run for real (default is noop)')
+
+    parser_get = subparsers.add_parser('get', help='Get the translations')
+    parser_get.add_argument('-d', '--dir', dest="dir", required=True, help='Path to English source directory of file')
+    parser_get.add_argument('-o', '--outputDir', dest="outputDir", required=True,
+                            help='Output directory where to save translated files. Stores each translation in their own sub-directory.')
+    parser_get.add_argument('-u', '--uriPath', dest="uriPath", help='File URI path used in Smartling system')
+    parser_get.add_argument('-l', '--locale', dest="locale", help='Locale to download (default is all)')
+    parser_get.add_argument('--run', dest="run", action='store_true', help='Run for real (default is noop)')
+
+    parser_import = subparsers.add_parser('import', help='Import translations')
+    parser_import.add_argument('-d', '--dir', dest="dir", required=True, help='Path to translations directory or file')
+    parser_import.add_argument('-u', '--uriPath', dest="uriPath", help='File URI path used in Smartling system')
+    parser_import.add_argument('-l', '--locale', dest="locale", required=True, help='Locale to import')
+    parser_import.add_argument('-o', '--overwrite', dest="overwrite", action='store_true', help='Overwrite previous translations')
+    parser_import.add_argument('--run', dest="run", action='store_true', help='Run for real (default is noop)')
+
+    args = parser.parse_args()
+
+    config = ConfigParser.ConfigParser()
+    # Allow case sensitive configuration keys
+    config.optionxform = str
+    config.read(args.configFile)
+
+    if not args.uriPath and config.has_section("smartling"):
+        args.uriPath = config.get("smartling", "uriPath")
+
+    # Default uriPath
+    if not args.uriPath:
+        args.uriPath = "/files"
+
+    # Fix URI path to include trailing slash (expected)
+    if not args.uriPath.endswith('/'):
+        args.uriPath += "/"
+
+    if not args.apiKey and config.has_section("smartling"):
+        args.apiKey = config.get("smartling", "apiKey")
+
+    if not args.projectId and config.has_section("smartling"):
+        args.projectId = config.get("smartling", "projectId")
+
+    if not args.apiKey or not args.projectId:
+        raise ValueError("Smartling API Key and Project ID are required")
+
+    if config.has_section("locales"):
+        locales = {}
+        for name, value in config.items("locales"):
+            locales[name] = value
+        args.localeMap = locales
+
+    if args.sub_parser == "upload":
+        directives = None
+        if config.has_section("directives"):
+            directives = config.items("directives")
+        uploadSource(args, directives)
+
+    if args.sub_parser == "get":
+        getTranslations(args)
+
+    if args.sub_parser == "import":
+        directives = None
+        if config.has_section("directives"):
+            directives = config.items("directives")
+        importTranslations(args, directives)
+
+
+main()
+
